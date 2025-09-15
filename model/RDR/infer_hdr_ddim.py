4092bda1bc656f476c3c2f8cc6e63be9805ae935
CodeHome <103580465+CodeSet1@users.noreply.github.com>
2025-09-15 22:05:56 +0800

    Update and rename infer_rdr_ddim.py to infer_hdr_ddim.py

--- model/RDR/infer_hdr_ddim.py
+++ model/RDR/infer_hdr_ddim.py
@@ -13,7 +13,7 @@ import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', type=str, default='config/Diff_RDA_val.json',
+    parser.add_argument('-c', '--config', type=str, default='config/Diff_HDR_val.json',
                         help='JSON file for configuration')
     parser.add_argument('-p', '--phase', type=str, choices=['val'], help='val(generation)', default='val')
     parser.add_argument('-gpu', '--gpu_ids', type=str, default=None)
